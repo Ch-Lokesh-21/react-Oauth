2d4dfcc0278587add414ff090e5e46421799c829
Ch Lokesh <lokeshchirumamilla2104@gmail.com>
2024-10-16 12:01:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import {jwtDecode} from "jwt-decode";
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleLoginSuccess = (credentialResponse) => {
+    const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+    setUser(credentialResponseDecoded);
+    console.log(credentialResponseDecoded);
+  };
+
+  const handleLogout = () => {
+    googleLogout();
+    setUser(null);
+    console.log("Logout Successful");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <div className="box"> 
+          <h2><p>Hello, {user.name}!</p></h2>
+          <p>Email: {user.email}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <GoogleLogin
+          onSuccess={handleLoginSuccess} 
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      )}
     </div>
   );
 }
 
 export default App;
+
